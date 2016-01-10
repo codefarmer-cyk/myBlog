fc3a0906bb634f7f015b3ec00bbc0394595e7261
codefarmer-cyk <594701648@qq.com>
2016-01-10 22:02:41 +0800

    update

--- src/main/java/com/chenyikui/blog/controller/HomeController.java
+++ src/main/java/com/chenyikui/blog/controller/HomeController.java
@@ -2,38 +2,62 @@ package com.chenyikui.blog.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.chenyikui.blog.model.Post;
+import com.chenyikui.blog.service.PostService;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
-	
+	@Autowired
+	private PostService postService;
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+		List<Post> posts = postService.getAllPost();
+		model.addAttribute("posts", posts);
+		model.addAttribute("serverTime", formattedDate);
+
 		return "home";
 	}
-	
+
+	@RequestMapping(value = "/about", method = RequestMethod.GET)
+	public String about(Model model) {
+		return "about";
+	}
+
+	@RequestMapping(value = "/post", method = RequestMethod.GET)
+	public String post(Model model) {
+		List<Post> posts = postService.getAllPost();
+		model.addAttribute("posts", posts);
+		return "post";
+	}
+
+	@RequestMapping(value = "/contact", method = RequestMethod.GET)
+	public String contact(Model model) {
+		return "contact";
+	}
 }
